378c50df0fb6d1c2846e432755e3392ed27d9840
Alvaro Luca Riccardi Roca de Togores <101105168+Lucarrdt@users.noreply.github.com>
2023-03-12 06:55:33 -0700

    IsTie method and updated values in the UI and the game logic class

--- TicTacToeProjectRiccardi/Game.cs
+++ TicTacToeProjectRiccardi/Game.cs
@@ -2,11 +2,11 @@
 
 public class Game
 {
-    private readonly int[,] _board = new int[3, 3]; //This represents the board
+    private readonly int[,] _board = new int[5, 5];
 
     public int[,] Board => _board;
     
-    public bool IsGameOver { get; private set; } 
+    public bool IsGameOver { get; private set; }
     
     public int Winner { get; private set; }
     
@@ -28,26 +28,47 @@ public class Game
             IsGameOver = true;
             Winner = player;
         }
+        else if (IsTie())
+        {
+            IsGameOver = true;
+        }
     }
-
     private bool CheckForWinner()
     {
-        //Checking rows
-        for (int i = 0; i < 3; i++)
+        // Checking rows
+        for (int i = 0; i < 4; i++)
         {
-            if (_board[i, 0] == _board[i, 1] && _board[i, 1] == _board[i, 2] && _board[i, 0] != 0)
-                return true;
-
-            if (_board[0, i] == _board[1, i] && _board[1, i] == _board[2, i] && _board[0, i] != 0)
-                return true;
-
-            if (_board[0, 0] == _board[1, 1] && _board[1, 1] == _board[2, 2] && _board[0, 0] != 0)
-                return true;
-            
-            if (_board[0, 2] == _board[1, 1] && _board[1, 1] == _board[2, 0] && _board[0, 2] != 0)
+            if (_board[i, 0] == _board[i, 1] && _board[i, 1] == _board[i, 2] && _board[i, 2] == _board[i, 3] && _board[i, 0] != 0)
                 return true;
         }
 
+        // Checking columns
+        for (int j = 0; j < 3; j++)
+        {
+            if (_board[0, j] == _board[1, j] && _board[1, j] == _board[2, j] && _board[2, j] == _board[3, j] && _board[0, j] != 0)
+                return true;
+        }
+
+        // Checking diagonals
+        if (_board[0, 0] == _board[1, 1] && _board[1, 1] == _board[2, 2] && _board[2, 2] == _board[3, 3] && _board[0, 0] != 0)
+            return true;
+
+        if (_board[0, 3] == _board[1, 2] && _board[1, 2] == _board[2, 1] && _board[2, 1] == _board[3, 0] && _board[0, 3] != 0)
+            return true;
+
         return false;
+    }
+    private bool IsTie()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (_board[i, j] == 0)
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
